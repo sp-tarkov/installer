2eb9197e376e56cbdc1c51148b52ab66d76ad01b
waffle.lord <waffle.lord@hotmail.com>
2023-07-29 14:26:55 -0400

    udpate prechecks

--- SPTInstaller/Installer Tasks/PreChecks/FreeSpacePreCheck.cs
+++ SPTInstaller/Installer Tasks/PreChecks/FreeSpacePreCheck.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using SPTInstaller.Helpers;
 using SPTInstaller.Models;
 
@@ -13,13 +14,35 @@ public class FreeSpacePreCheck : PreCheckBase
         _internalData = internalData;
     }
 
-    public override async Task<bool> CheckOperation()
+    public override async Task<PreCheckResult> CheckOperation()
     {
-        if (_internalData.OriginalGamePath is null || _internalData.TargetInstallPath is null)
-        {
-            return false;
-        }
+        if (_internalData.OriginalGamePath is null)
+            return PreCheckResult.FromError("Could not find EFT game path");
 
-        return DirectorySizeHelper.CheckAvailableSize(_internalData.OriginalGamePath, _internalData.TargetInstallPath);
+        if (_internalData.TargetInstallPath is null)
+            return PreCheckResult.FromError("Could not find install target path");
+
+        try
+        {
+            var eftSourceDirectoryInfo = new DirectoryInfo(_internalData.OriginalGamePath);
+            var installTargetDirectoryInfo = new DirectoryInfo(_internalData.TargetInstallPath);
+
+            var eftSourceDirSize = DirectorySizeHelper.GetSizeOfDirectory(eftSourceDirectoryInfo);
+            var availableSize = DriveInfo.GetDrives().FirstOrDefault(d => d.Name.ToLower() == installTargetDirectoryInfo.Root.Name.ToLower())?.AvailableFreeSpace ?? 0;
+
+            var availableSpaceMessage = $"Available Space: {DirectorySizeHelper.SizeSuffix(availableSize, 2)}";
+            var requiredSpaceMessage = $"Space Required for EFT Client: {DirectorySizeHelper.SizeSuffix(eftSourceDirSize, 2)}";
+
+            if (eftSourceDirSize > availableSize)
+            {
+                return PreCheckResult.FromError($"Not enough free space on {installTargetDirectoryInfo.Root.Name} to install SPT\n\n{availableSpaceMessage}\n{requiredSpaceMessage}");
+            }
+
+            return PreCheckResult.FromSuccess($"There is enough space available on {installTargetDirectoryInfo.Root.Name} to install SPT.\n\n{availableSpaceMessage}\n{requiredSpaceMessage}");
+        }
+        catch (Exception ex)
+        {
+            return PreCheckResult.FromException(ex);
+        }
     }
 }
