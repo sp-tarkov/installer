bb4c535244710f596904cd4a3c46ef50bf13992b
waffle.lord <waffle.lord@hotmail.com>
2024-03-26 16:11:14 -0400

    new update control WIP

---
+++ SPTInstaller/CustomControls/UpdateButton.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SPTInstaller.CustomControls;
+
+public partial class UpdateButton : UserControl
+{
+    public UpdateButton()
+    {
+        InitializeComponent();
+    }
+    
+    
+    
+    // InfoText="{Binding UpdateInfo.UpdateInfoText}"
+    // ShowUpdateCard="{Binding UpdateInfo.ShowCard}"
+    // NotNowCommand="{Binding DismissUpdateCommand}"
+    // UpdateInstallerCommand="{Binding UpdateInstallerCommand}"
+    // Updating="{Binding UpdateInfo.Updating}"
+    // DownloadProgress="{Binding UpdateInfo.DownloadProgress}"
+    // IndeterminateProgress="{Binding UpdateInfo.CheckingForUpdates}"
+    // UpdateAvailable="{Binding UpdateInfo.UpdateAvailable}"
+}
