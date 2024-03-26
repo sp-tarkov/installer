992931b15c6bae12ef4542914e2089cb0c860c3e
waffle.lord <waffle.lord@hotmail.com>
2024-03-26 18:59:22 -0400

    add properties to control

--- SPTInstaller/CustomControls/UpdateButton.axaml.cs
+++ SPTInstaller/CustomControls/UpdateButton.axaml.cs
@@ -1,6 +1,6 @@
-﻿using Avalonia;
+﻿using System.Windows.Input;
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
 namespace SPTInstaller.CustomControls;
 
@@ -10,15 +10,76 @@ public partial class UpdateButton : UserControl
     {
         InitializeComponent();
     }
-    
-    
-    
-    // InfoText="{Binding UpdateInfo.UpdateInfoText}"
-    // ShowUpdateCard="{Binding UpdateInfo.ShowCard}"
-    // NotNowCommand="{Binding DismissUpdateCommand}"
-    // UpdateInstallerCommand="{Binding UpdateInstallerCommand}"
-    // Updating="{Binding UpdateInfo.Updating}"
-    // DownloadProgress="{Binding UpdateInfo.DownloadProgress}"
-    // IndeterminateProgress="{Binding UpdateInfo.CheckingForUpdates}"
-    // UpdateAvailable="{Binding UpdateInfo.UpdateAvailable}"
+
+    public static readonly StyledProperty<string> InfoTextProperty = AvaloniaProperty.Register<UpdateButton, string>(
+        "InfoText");
+
+    public string InfoText
+    {
+        get => GetValue(InfoTextProperty);
+        set => SetValue(InfoTextProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> ShowProperty = AvaloniaProperty.Register<UpdateButton, bool>(
+        "Show");
+
+    public bool Show
+    {
+        get => GetValue(ShowProperty);
+        set => SetValue(ShowProperty, value);
+    }
+
+    public static readonly StyledProperty<ICommand> DismissCommandProperty = AvaloniaProperty.Register<UpdateButton, ICommand>(
+        "DismissCommand");
+
+    public ICommand DismissCommand
+    {
+        get => GetValue(DismissCommandProperty);
+        set => SetValue(DismissCommandProperty, value);
+    }
+
+    public static readonly StyledProperty<ICommand> UpdateCommandProperty = AvaloniaProperty.Register<UpdateButton, ICommand>(
+        "UpdateCommand");
+
+    public ICommand UpdateCommand
+    {
+        get => GetValue(UpdateCommandProperty);
+        set => SetValue(UpdateCommandProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> UpdatingProperty = AvaloniaProperty.Register<UpdateButton, bool>(
+        "Updating");
+
+    public bool Updating
+    {
+        get => GetValue(UpdatingProperty);
+        set => SetValue(UpdatingProperty, value);
+    }
+
+    public static readonly StyledProperty<int> DownloadProgressProperty = AvaloniaProperty.Register<UpdateButton, int>(
+        "DownloadProgress");
+
+    public int DownloadProgress
+    {
+        get => GetValue(DownloadProgressProperty);
+        set => SetValue(DownloadProgressProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsIndeterminateProperty = AvaloniaProperty.Register<UpdateButton, bool>(
+        "IsIndeterminate");
+
+    public bool IsIndeterminate
+    {
+        get => GetValue(IsIndeterminateProperty);
+        set => SetValue(IsIndeterminateProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> UpdateAvailableProperty = AvaloniaProperty.Register<UpdateButton, bool>(
+        "UpdateAvailable");
+
+    public bool UpdateAvailable
+    {
+        get => GetValue(UpdateAvailableProperty);
+        set => SetValue(UpdateAvailableProperty, value);
+    }
 }
